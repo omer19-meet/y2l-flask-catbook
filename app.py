cb78c7f5e6c9612061357f8851d7aa728f092c32
aboud20-meet <aboud20@meet.mit.edu>
2018-11-08 18:50:05 +0200

    not finished

--- app.py
+++ app.py
@@ -10,5 +10,16 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cats/<int:id>')
+def get_cat_route(id):
+    catsp = get_all_cats()
+    for cat in catsp :
+        if cat.id == id :
+            obj = cat
+            # return render_template('cat.html', obj= cat)
+
+    # Get cat with id from database
+    return render_template('cat.html', cat=obj )
+
 if __name__ == '__main__':
    app.run(debug = True)
