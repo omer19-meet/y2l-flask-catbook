dcb0408e90b5cbcf2d2996ea1b693b9606005e7b
aboud20-meet <aboud20@meet.mit.edu>
2019-04-04 00:18:11 +0300

    m

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 from flask import render_template
-from database import get_all_cats
+from database import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -12,14 +12,23 @@ def catbook_home():
 
 @app.route('/cats/<int:id>')
 def get_cat_route(id):
-    catsp = get_all_cats()
-    for cat in catsp :
-        if cat.id == id :
-            obj = cat
-            # return render_template('cat.html', obj= cat)
+    cat_1 = get_cat_by_id(id)
+    return render_template('cat.html', cat= cat_1)
 
     # Get cat with id from database
-    return render_template('cat.html', cat=obj )
+    # return render_template('cat.html', cat_1 )
+
+@app.route('/creat', methods=['GET', 'POST'])
+def create_new_cat():
+	if request.method == 'POST':
+		return render_template('create_cat.html')
+	else: 
+		name = request.input['name']
+		votes=0
+		create_cat(name=name, votes=votes)
+		print("new cat added")
+		return render_template('respond.html')
+
 
 if __name__ == '__main__':
    app.run(debug = True)
--- database.py
+++ database.py
@@ -8,12 +8,33 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def create_cat(name):
-    cat_object = Cat(name=name)
+def create_cat(name, votes):
+    cat_object = Cat(name=name, votes=votes)
     session.add(cat_object)
     session.commit()
 
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+
+def get_cat_by_id(their_id):
+	cat = session.query(Cat).filter_by(id=their_id).first()
+	return cat
+
+def count_votes(id):
+	cat = session.query(Cat).filter_by(id=id).first()
+	# session.commit()
+	cat.votes += 1
+	session.commit()																																																																		
+	# return cat.votes
+# the goa;l of the function is 
+# 1) get the cat from the db
+# 2) increase the votes by 1
+# 3) save it to t
+
+# create_cat("Mizti", 0)
+# create_cat("Miyahoooo", 0)
+
     
+# def add_vote():
+	
